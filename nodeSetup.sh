11a6998dde72635acd1d93d1a272a9dca007eb50
jonathonlefler <lefler.t.jonathon@gmail.com>
2019-02-23 23:52:24 +0000

    fixed file paths and perms

--- nodeSetup.sh
+++ nodeSetup.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 echo "Initializing $1"
+cd /home/pi
 echo "Downloading git"
 apt install git
 echo "Downloading LMIC"
 git clone https://github.com/wklenk/lmic-rpi-lora-gps-hat.git
+chown -R pi:pi lmic-rpi-lora-gps-hat
 echo "Downloading WiringPi"
 git clone git://git.drogon.net/wiringPi
 echo "Installing WiringPi"
@@ -11,6 +13,7 @@ cd /home/pi/wiringPi
 git pull origin
 ./build
 cd
+chown -R pi:pi wiringPi
 echo "Enabling SSH."
 systemctl enable ssh
 echo "Changing keyboard layout"
