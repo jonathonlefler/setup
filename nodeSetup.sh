cab9bb54124dd79701e7064f7d9b81c7fd514bf9
jonathonlefler <lefler.t.jonathon@gmail.com>
2019-02-24 01:44:15 +0000

    fixed serail enablement

--- nodeSetup.sh
+++ nodeSetup.sh
@@ -11,17 +11,20 @@ echo "Installing pip3"
 apt install python3-pip
 echo "Installing pyserial"
 pip3 install pyserial
+echo "Enabling serial UART"
+sed -i -e 's/ console=serial0,115200//g' /boot/cmdline.txt
+echo 'enable_usart=1' >> /boot/config.txt
 echo "Downloading LMIC"
 git clone https://github.com/wklenk/lmic-rpi-lora-gps-hat.git
 chown -R pi:pi lmic-rpi-lora-gps-hat
 echo "Downloading WiringPi"
 git clone git://git.drogon.net/wiringPi
+chown -R pi:pi wiringPi
 echo "Installing WiringPi"
 cd /home/pi/wiringPi
 git pull origin
 ./build
-cd
-chown -R pi:pi wiringPi
+cd /home/pi
 echo "Enabling SSH."
 systemctl enable ssh
 echo "Changing keyboard layout"
